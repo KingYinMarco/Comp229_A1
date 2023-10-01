5b04b626d0c62d7419d66a0d25477dcbf454e00c
Kingyin pang <pangkingyin7899@gmail.com>
2023-10-01 07:09:05 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+// Home page
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+// About page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+// Projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+// Services page
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+// Contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
